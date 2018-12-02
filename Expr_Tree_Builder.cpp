a3cfd7c0c3d35e474a16054510057ff50a189650
Jared Schoolcraft <jascho@tesla.cs.iupui.edu>
2018-12-01 23:11:29 -0500

    Fixed seg-fault for normal precidence but still seg-fault if I use parenthesis.

--- Expr_Tree_Builder.cpp
+++ Expr_Tree_Builder.cpp
@@ -46,6 +46,7 @@ void Expr_Tree_Builder::build_number (int n)
 		nodes[iterator-1]->set_left(tree.pop());
 		tree.push(nodes[iterator-1]);
 	}
+	check_precidence(nodes, iterator);
 	std::cout<<"Made it around/through the if loop."<<'\n';
 }
 
@@ -63,7 +64,7 @@ void Expr_Tree_Builder::build_add_operator(void)
 		std::cout<<"Setting add node in array."<<'\n';
 		iterator=iterator+1;
 		std::cout<<"Incrimenting iterator."<<'\n';
-		check_precidence(nodes, iterator);
+		//check_precidence(nodes, iterator);
 		std::cout<<"Checked precidence."<<'\n';
 	}
 	else
@@ -71,7 +72,7 @@ void Expr_Tree_Builder::build_add_operator(void)
 		op->set_parenthesis(true);
 		p_nodes.set(p_iterator, op);
 		p_iterator=p_iterator+1;
-		check_precidence(p_nodes, p_iterator);
+		//check_precidence(p_nodes, p_iterator);
 	}
 }
 void Expr_Tree_Builder::build_sub_operator(void)
@@ -84,14 +85,14 @@ void Expr_Tree_Builder::build_sub_operator(void)
 	{
 		nodes.set(iterator, op);
 		iterator=iterator+1;
-		check_precidence(nodes, iterator);
+		//check_precidence(nodes, iterator);
 	}
 	else
 	{
 		op->set_parenthesis(true);
 		p_nodes.set(p_iterator, op);
 		p_iterator=p_iterator +1;
-		check_precidence(p_nodes, p_iterator);
+		//check_precidence(p_nodes, p_iterator);
 	}
 }
 
@@ -105,14 +106,14 @@ void Expr_Tree_Builder::build_mul_operator(void)
 	{
 		nodes.set(iterator, op);
 		iterator=iterator+1;
-		check_precidence(nodes, iterator);
+		//check_precidence(nodes, iterator);
 	}
 	else
 	{
 		op->set_parenthesis(true);
 		p_nodes.set(p_iterator, op);
 		p_iterator=p_iterator +1;
-		check_precidence(p_nodes, p_iterator);
+		//check_precidence(p_nodes, p_iterator);
 	}
 }
 void Expr_Tree_Builder::build_div_operator(void)
@@ -125,14 +126,14 @@ void Expr_Tree_Builder::build_div_operator(void)
 	{
 		nodes.set(iterator, op);
 		iterator=iterator+1;
-		check_precidence(nodes, iterator);
+		//check_precidence(nodes, iterator);
 	}
 	else
 	{
 		op->set_parenthesis(true);
 		p_nodes.set(p_iterator, op);
 		p_iterator=p_iterator+1;
-		check_precidence(p_nodes, p_iterator);
+		//check_precidence(p_nodes, p_iterator);
 	}
 }
 
@@ -146,14 +147,14 @@ void Expr_Tree_Builder::build_mod_operator(void)
 	{
 		nodes.set(iterator, op);
 		iterator=iterator+1;
-		check_precidence(nodes, iterator);
+		//check_precidence(nodes, iterator);
 	}
 	else
 	{
 		op->set_parenthesis(true);
 		p_nodes.set(p_iterator, op);
 		p_iterator=p_iterator+1;
-		check_precidence(p_nodes, p_iterator);
+		//check_precidence(p_nodes, p_iterator);
 	}
 }
 
@@ -175,7 +176,7 @@ void Expr_Tree_Builder::build_close_parenthesis(void)
 		begin=begin+1;
 	}
 	nodes=p_nodes;
-	(*this).check_precidence(nodes, iterator);
+	check_precidence(nodes, iterator);
 }
 
 void Expr_Tree_Builder::check_precidence(Array <Binary_Expr_Node*> &nodes, int iterator)
@@ -185,14 +186,20 @@ void Expr_Tree_Builder::check_precidence(Array <Binary_Expr_Node*> &nodes, int i
 	{
 		std::cout<<"Inside if statement (check_precidence)."<<'\n';
 		int check=iterator-1;
-		while(nodes[check-1]->get_precidence()<nodes[check]->get_precidence()&&nodes[check-1]->get_parenthesis()!=true&&check!=0)
+		while(check!=0&&nodes[check-1]->get_precidence()<nodes[check]->get_precidence()&&nodes[check-1]->get_parenthesis()!=true)
 		{
 			std::cout<<"Inside while loop (check_precidence)."<<'\n';
 			Binary_Expr_Node* temp=nodes[check-1];
+			std::cout<<"Created temp."<<'\n';
 			nodes[check-1]=nodes[check];
 			nodes[check]=temp;
+			std::cout<<"Switched nodes in array."<<'\n';
 			nodes[check-1]->set_left(nodes[check]->get_right());
+			nodes[check]->set_right(nodes[check]->get_left());
 			nodes[check]->set_left(nodes[check-1]);
+			std::cout<<"Set new children keys."<<'\n';
+			check=check-1;
+			std::cout<<"Deincrement check."<<"\n";
 		}
 	}
 }
